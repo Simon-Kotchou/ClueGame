2c0f026550e24475804079f7a726df5b2543aa3e
Simon Kotchou <simonkotchou@mymail.mines.edu>
2019-10-07 22:02:50 -0600

    Created BoardClass, as well as IntBoard and filled intBoard with function stubs like getAdjList and calcTargets

---
+++ src/experiment/BoardCell.java
@@ -0,0 +1,11 @@
+package experiment;
+
+public class BoardCell {
+	private int row;
+	private int col;
+	
+	public BoardCell(int r, int c) {
+		this.row = r;
+		this.col = c;
+	}
+}
---
+++ src/experiment/IntBoard.java
@@ -0,0 +1,34 @@
+package experiment;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class IntBoard {
+	private Map<BoardCell, Set<BoardCell>> adjMatrix;			//Map that includes every boardcell on grid and a set of adjacent cells
+	private Set<BoardCell> visited;								//set of boardcells that have been visited by a function
+	private Set<BoardCell> targets;								//Set of boardcells that are the targets when moving a player
+	private BoardCell[][] grid;									//A grid representing the game board
+	
+	public IntBoard() {									//default constructor that sets up adjMatrix using calcAdjacencies()
+		calcAdjacencies();
+	}
+	
+	public void calcAdjacencies() {							//Function that will fill adjMatrix with every boardcell and the boardcells adjacent
+		return;
+	}
+	
+	public Set<BoardCell> getAdjList(){								//getter for adjMatrix
+		return null;
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength){		//Function that calculates the targets for a move and stores them in set of targets
+		return;
+	}
+	
+	public Set<BoardCell> getTargets(){									//getter for targets
+		return null;
+	}
+	
+	
+}
